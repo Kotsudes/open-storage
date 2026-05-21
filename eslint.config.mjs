bc11f336228078e3d7655ee1f61a6401715856d1
Kotsudes <ridgelowao@gmail.com>
2026-05-21 17:22:18 +0200

    feat: added web app with Next.js and Tailwind CSS
    refractor: changed eslint config to support Next.js and Tailwind CSS

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export const baseConfig = defineConfig([
+    {
+        files: ["**/*.{js,mjs,cjs,ts,mts,cts,tsx}"],
+        plugins: { js },
+        extends: [js.configs.recommended],
+        languageOptions: {
+            globals: globals.browser,
+        },
+        ignores: [
+            "coverage",
+            "**/public",
+            "**/dist",
+            "pnpm-lock.yaml",
+            "pnpm-workspace.yaml",
+            ".next",
+        ],
+    },
+
+    tseslint.configs.recommended,
+    eslintPluginPrettierRecommended,
+]);
+
+export default baseConfig;
